1f063feff649f3aeb9cfa79346536e841511b07f
EarlV <qekgvillamor@tip.edu.ph>
2022-12-12 03:40:53 +0800

    update

---
+++ admin-openrc.sh
@@ -0,0 +1,10 @@
+#Penas
+export OS_USERNAME=admin
+export OS_PASSWORD=earlv
+export OS_PROJECT_NAME=admin
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_AUTH_URL=http://ubuntu_server:5000/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_PROJECT_DOMAIN_ID=Default
+export OS_USER_DOMAIN_ID=Default
